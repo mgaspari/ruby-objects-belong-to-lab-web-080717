35878e9c2f2a43682f74722cdb9d4e2ad2256e72
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-10 15:52:10 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+attr_accessor :title, :author
+
+def author
+  @author
+end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+attr_accessor :title,  :artist
+
+def initialize
+@title
+
+end
+
+def artist
+  @artist
+end
+end
